ce5d118e65dec6caec46903470becf1780bf54fe
Marcelo Basílio <marcelocbasilio@gmail.com>
2020-04-28 02:45:11 -0300

    First Commit!

---
+++ js/principal.js
@@ -0,0 +1,7 @@
+function liga() {
+    document.querySelector('#lampada').src = 'img/lampada-apagada.gif';
+}
+
+function desliga() {
+    document.querySelector('#lampada').src = 'img/lampada-acesa.gif';
+}
